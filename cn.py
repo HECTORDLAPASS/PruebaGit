470dd75c145b739f1cff49e274ae777548f68ea8
HECTORDELAPASS <hectorjvpass2@gmail.com>
2025-03-14 13:25:27 -0600

    base exitosa

--- cn.py
+++ cn.py
@@ -1,13 +1,29 @@
 import psycopg2
+
+query = "SELECT * FROM empleados;"
+
 try:
     conn = psycopg2.connect(
         host="localhost",
-        user="admin",
+        user="postgres",
         password="admin",
         database="Narad",
         port="5432"
     )
-    print("conectado a la base de datos")
+    print("Conectado a la base de datos")
+
+    cur = conn.cursor()
+    cur.execute(query)
+    resultados = cur.fetchall()
+
+    for fila in resultados:
+        print(fila)
+
+    cur.close()
 
 except (Exception, psycopg2.Error) as error:
-    print("Error while connecting to PostgreSQL", error)
+    print("Error al conectar a PostgreSQL", error)
+finally:
+    conn.close()
+    print("conexion cerrada")
+
