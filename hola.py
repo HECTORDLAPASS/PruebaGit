11f54f5157426d8c06ea483eb5d9af3abe423a15
HECTORDELAPASS <hectorjvpass2@gmail.com>
2025-03-14 11:30:36 -0600

    modificique el formulario hola. ahora hace preguntas con el imput

--- hola.py
+++ hola.py
@@ -1,3 +1,25 @@
 print("holas")
 
-print(f"hola culeros {200*34}")
+nombre = input("Tu nombre: ")
+telefono = input("Tu telefono: ")
+
+i  =1
+while i < 2:
+    opcion = input("1. para ver nombre \n2. para ver telefono \n3. para ver todo \n...")
+
+    if opcion == "1":
+        print("tu nombre es",nombre)
+        break
+    elif opcion == "2":
+        print("tu telefono es",telefono)
+        break
+    elif opcion == "3":
+        print("tu nombre es",nombre, "tu telefono es",telefono)
+        break
+    else:
+        print("opcion invalida")
+
+
+
+
+
